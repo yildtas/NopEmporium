e2530e4b4c76f54ac0fa64a64e63ed14a87acd32
askeroglu <askeroglu@gmail.com>
2020-07-13 16:43:02 +0300

    Payment
    place_payment_by_customer_id

---
+++ Plugins/Nop.Plugin.Api/Controllers/PaymentController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core.Domain.Catalog;
+using Nop.Plugin.Api.Delta;
+using Nop.Plugin.Api.Factories;
+using Nop.Plugin.Api.Helpers;
+using Nop.Plugin.Api.ModelBinders;
+using Nop.Plugin.Api.Services;
+using Nop.Services.Catalog;
+using Nop.Services.Customers;
+using Nop.Services.Discounts;
+using Nop.Services.Localization;
+using Nop.Services.Logging;
+using Nop.Services.Media;
+using Nop.Services.Security;
+using Nop.Services.Seo;
+using Nop.Services.Stores;
+using Stripe;
+using System.Net;
+
+namespace Nop.Plugin.Api.Controllers
+{
+    using JSON.Serializers;
+    using Nop.Plugin.Api.Models.ShoppingCart;
+
+    //[ApiAuthorize(Policy = JwtBearerDefaults.AuthenticationScheme, AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class PaymentController : BaseApiController
+    {
+        private readonly ICategoryApiService _categoryApiService;
+        private readonly ICategoryService _categoryService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IFactory<Category> _factory; 
+        private readonly IDTOHelper _dtoHelper;
+
+        public PaymentController(ICategoryApiService categoryApiService,
+            IJsonFieldsSerializer jsonFieldsSerializer,
+            ICategoryService categoryService,
+            IUrlRecordService urlRecordService,
+            ICustomerActivityService customerActivityService,
+            ILocalizationService localizationService,
+            IPictureService pictureService,
+            IStoreMappingService storeMappingService,
+            IStoreService storeService,
+            IDiscountService discountService,
+            IAclService aclService,
+            ICustomerService customerService,
+            IFactory<Category> factory,
+            IDTOHelper dtoHelper) : base(jsonFieldsSerializer, aclService, customerService, storeMappingService, storeService, discountService, customerActivityService, localizationService,pictureService)
+        {
+            _categoryApiService = categoryApiService;
+            _categoryService = categoryService;
+            _urlRecordService = urlRecordService;
+            _factory = factory;
+            _dtoHelper = dtoHelper;
+        }
+
+        [HttpPost]
+        [Route("/api/place_payment_by_customer_id")]
+        public virtual IActionResult PlacePaymentByCustomerId([ModelBinder(typeof(JsonModelBinder<PaymentModel>))] Delta<PaymentModel> paymentModel)
+        {
+
+            StripeConfiguration.ApiKey = "sk_test_jqqXw7n0PjEISJ9gbu0YadNx";
+
+            var chargeOptions = new ChargeCreateOptions
+            {
+                Amount = 2000,
+                Currency = "usd",
+                Source = paymentModel.Dto.StripeToken,
+                Description = "My First Test Charge (created for API docs)",
+                ReceiptEmail = paymentModel.Dto.ReceiptEmail,
+                Customer = paymentModel.Dto.CustomerId
+            };
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            var service = new ChargeService();
+
+            try
+            {
+                var stripeCharge = service.Create(chargeOptions);
+            }
+            catch (StripeException stripeException)
+            {
+                //Debug.WriteLine(stripeException.Message);
+                return BadRequest();
+            }
+
+            //return RedirectToAction("Confirmation");
+            return Ok();
+        }
+    }
+}
---
+++ Plugins/Nop.Plugin.Api/Models/Payment/PaymentModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
+using Nop.Core.Domain.Catalog;
+using Nop.Plugin.Api.Models.Media;
+using Nop.Web.Framework.Models;
+using System.Collections.Generic;
+
+namespace Nop.Plugin.Api.Models.ShoppingCart
+{
+    [JsonObject(Title = "Payment")]
+    public partial class PaymentModel : BaseNopModel
+    {
+        public PaymentModel()
+        {
+
+        }
+
+        [JsonProperty("Amount")]
+        public double Amount { get; set; }
+
+        [JsonProperty("CustomerId")]
+        public string CustomerId { get; set; }
+
+        [JsonProperty("StripeToken")] 
+        public string StripeToken { get; set; }
+
+        [JsonProperty("Currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("Description")]
+        public string Description { get; set; }
+
+        [JsonProperty("ReceiptEmail")]
+        public string ReceiptEmail { get; set; }
+    }
+}
